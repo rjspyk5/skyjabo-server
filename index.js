e5efac7d9868dfb8602816449aa578c84a6096db
rjspyk5 <rjspyk5@gmail.com>
2024-11-28 03:56:12 +0600

    create mvc stucture and create authcontroller

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const { register } = require("./controllers/authController");
 const app = express();
 const port = process.env.PORT ?? 5000;
 require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => res.send("Test"));
+app.post("/register", register);
 app.listen(port, () => console.log("server is running"));
