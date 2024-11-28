cdb14dcd5f11c29a61e5b8c8c2605fc9f14d5a67
rjspyk5 <rjspyk5@gmail.com>
2024-11-28 23:50:27 +0600

    login and register api completef

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,50 @@
 const bcrypt = require("bcrypt");
 const { findUserByEmail, createUser } = require("../model/userModel");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const userDetails = req.body;
-  const isAlreadyHaveAccount = await findUserByEmail(userDetails.email);
-  if (isAlreadyHaveAccount) {
+  const isUser = await findUserByEmail(userDetails.email);
+  if (isUser) {
     return res.send({ message: "User already exists" });
   }
   const hashedPass = await bcrypt.hash(userDetails?.password, 10);
-  // todo: save into database if ok then send response ok
+
   const result = await createUser("test", userDetails?.email, hashedPass, 1);
   if (result?._id) {
     return res.send({ message: "User Created Successflly,Now you can login" });
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  // chechking valid user or not
+  const isUser = await findUserByEmail(email);
+  if (!isUser) {
+    return res.send({ message: "Couldn't find any account with this email" });
+  }
+  // check password
+  const isMatchedPassword = await bcrypt.compare(password, isUser.password);
+  if (!isMatchedPassword) {
+    return res.send({ message: "Password didn't match,try again" });
+  }
+
+  const token = jwt.sign(
+    {
+      userId: isUser._id,
+      role: isUser.role,
+    },
+    process.env.ACCESS_TOKEN,
+    { expiresIn: "7d" }
+  );
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      maxAge: 3600000,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ message: "Login Successfull" });
+};
+module.exports = { register, login };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { register } = require("./controllers/authController");
+const { register, login } = require("./controllers/authController");
 const connectDb = require("./config/db");
 
 const app = express();
@@ -11,9 +11,15 @@ connectDb();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5174", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 // routes
 app.get("/", (req, res) => res.send("Test"));
 app.post("/register", register);
+app.post("/login", login);
 app.listen(port, () => console.log("server is running"));
